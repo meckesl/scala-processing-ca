08b1b1b16aa31c211759c928055f49b0b81fefb9
Louis Meckes <louis.meckes@gmail.com>
2015-08-15 23:11:00 +0200

    wip

--- src/main/scala/processing/Scala2dCA.scala
+++ src/main/scala/processing/Scala2dCA.scala
@@ -12,21 +12,19 @@ object Scala2dCA {
 
 class RunCA extends PApplet {
 
-  var iter = 0
   var gen, rule = Seq[Boolean]()
-  val res = 8
+  var iter = 0
+  val res = 16
 
   override def settings() {
     fullScreen()
   }
 
   override def setup() {
-    frameRate(100);
-    background(255, 255, 255)
-    noStroke()
     iter = 0
-    gen = 1 to width/res map (_ => Random.nextBoolean)
+    gen = 1 to width / res map (_ => Random.nextBoolean)
     rule = 1 to 8 map (_ => Random.nextBoolean)
+    frameRate(100);
   }
 
   override def keyPressed() {
@@ -35,25 +33,25 @@ class RunCA extends PApplet {
 
   override def draw() {
 
-    gen = computeCA(rule, gen)
+    gen = computeCA(gen, rule)
     gen.zipWithIndex.foreach {
       case (cell, index) => {
         if (cell) fill(0) else fill(255)
-        rect(index*res, iter*res, res, res)
+        rect(index * res, iter * res, res, res)
       }
     }
 
     iter = iter + 1
-    if (iter == height/res) setup
+    if (iter == height / res) setup
   }
 
 
-  def computeCA(rule: Seq[Boolean], gen: Seq[Boolean]): Seq[Boolean] = {
+  def computeCA(gen: Seq[Boolean], rule: Seq[Boolean]): Seq[Boolean] = {
 
     implicit def b2i(b: Boolean) = if (b) 1 else 0
 
     def computeChild(siblings: Seq[Boolean]): Boolean = {
-      require(siblings.length == 3, "Provide prev, cur, and next cell for parent generation")
+      require(siblings.length.equals(3), "Provide prev, cur, and next cell for parent generation")
       var n = 0;
       siblings.foreach {
         case (x) => {
@@ -64,11 +62,11 @@ class RunCA extends PApplet {
     }
 
     var newgen = Seq[Boolean]()
-    var prev, next = false;
+    var prev, next = false
     gen.zipWithIndex.foreach {
       case (cur, i) => {
         if (i == 0) {
-          prev = gen(gen.length - 1);
+          prev = gen.reverse.head;
           next = gen(i + 1)
         } else if (i == gen.length - 1) {
           next = gen.head;
